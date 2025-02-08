5c9bd41ba9ec0bbef86205ee79310ae3b5a3b67e
Nauqh <hodominhquan.work@gmail.com>
2025-02-08 18:42:55 +0700

    feat(sql output): add sign in page

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,8 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 const isPublicRoute = createRouteMatcher([
-  '/',
-  '/auth(.*)',
+  '/auth/sign-in(.*)',
+  '/auth/sign-up(.*)'
 ])
 
 export default clerkMiddleware(async (auth, request) => {
