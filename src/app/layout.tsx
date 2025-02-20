8e8b5df522c603def88dd7feb1c986394c1e3363
Nauqh <hodominhquan.work@gmail.com>
2025-02-20 13:20:02 +0700

    feat(problem): try add og image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
 		card: "summary_large_image",
 		site: "csassessment.it.com",
 		creator: "@nauqh",
-		images: ["https://csassessment.it.com/banner.png"], // Made URL consistent with OpenGraph
+		images: ["https://csassessment.it.com/banner.png"],
 	},
 };
 
