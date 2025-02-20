4157e78475501829f9ede3aafbf2a249fcb9ee4b
Nauqh <hodominhquan.work@gmail.com>
2025-02-20 15:04:47 +0700

    feat(problem): try add og image

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -132,7 +132,7 @@ export default function Navigation() {
 				<div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
 					<div className="flex flex-col gap-2">
 						<div className="flex justify-between items-center h-16">
-							<Link href="/" className="flex-shrink-0">
+							<Link href="/">
 								<Image
 									src="/logo.png"
 									alt="eExams Logo"
