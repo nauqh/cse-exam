bfa9431da26836da4c79b619f197303e3e97307f
Nauqh <hodominhquan.work@gmail.com>
2025-02-20 13:04:17 +0700

    feat(problem): add courses and assignment

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,9 @@ const manrope = Manrope({
 export const metadata: Metadata = {
 	title: "Coderschool e-Exam",
 	description: "Final Assessment Platform for Coderschool Students",
+	icons: {
+		icon: "/favicon.png",
+	},
 	metadataBase: new URL("https://csassessment.it.com"),
 	openGraph: {
 		type: "website",
